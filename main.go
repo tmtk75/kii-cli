ba70f0eb1b12dce26907ae696f0773a9e6c04500
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-06-24 22:24:27 +0900

    bump up version

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "kii-cli"
 	app.Usage = "KiiCloud command line interface"
-	app.Version = "0.0.3"
+	app.Version = "0.0.4"
 	app.Commands = Flatten([][]cli.Command{
 		LoginCommands,
 		LogCommands,
