4599a16cdec7a8e6834e497ea5555170d8243224
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-06-26 00:01:37 +0900

    show concept for practical I/O

--- users.go
+++ users.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-
 	"github.com/codegangsta/cli"
+	"io"
+	"os"
 )
 
 type UserCreationRequest struct {
@@ -17,8 +17,8 @@ func CreateUser(loginname string, password string) {
 	headers := globalConfig.HttpHeaders("application/json")
 	req := &UserCreationRequest{loginname, password}
 	res := HttpPostJson(path, headers, req)
-	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(body))
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
 }
 
 var UsersCommands = []cli.Command{
