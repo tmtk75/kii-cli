769a22f2fd1cbb63b9628961286f3d148fea4133
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-12-14 12:16:31 +0900

    log-url option

--- config.go
+++ config.go
@@ -171,6 +171,7 @@ func SetupFlags(app *cli.App) {
 		cli.StringFlag{Name: "client-secret", Value: "", Usage: "ClientSecret"},
 		cli.StringFlag{Name: "site", Value: "", Usage: "us,jp,cn,sg"},
 		cli.StringFlag{Name: "endpoint-url", Value: "", Usage: "Site URL"},
+		cli.StringFlag{Name: "log-url", Value: "", Usage: "Log URL"},
 		cli.BoolFlag{Name: "verbose", Usage: "Verbosely"},
 		cli.StringFlag{Name: "profile", Value: DEFAULT_PROFILE, Usage: "Profile name for ~/.kii/config"},
 		cli.BoolFlag{Name: "curl", Usage: "Print curl command saving body as a tmp file if body exists"},
