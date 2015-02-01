7030d315ee8b3a96ab2da82e2cc5447b7a039725
Tomotaka Sakuma <ktmtmks@gmail.com>
2015-02-01 14:08:08 +0900

    fix test case

--- time_test.go
+++ time_test.go
@@ -2,8 +2,8 @@ package kiicli
 
 import "testing"
 
-func Test_timeStringInUTCToLocalTime(t *testing.T) {
-	v, _ := timeStringInUTCToLocalTime("a")
+func Test_timeFromStringInUTC(t *testing.T) {
+	v, _ := timeFromStringInUTC("a")
 	expected := int64(-62135596800)
 	if v.Unix() != expected {
 		t.Errorf("expected %v, but %v", expected, v.Unix())
