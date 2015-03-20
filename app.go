672c28a0b24e41b82d11dcf2a03fcc7e00e26e05
Tomotaka Sakuma <ktmtmks@gmail.com>
2015-03-20 19:11:51 +0900

    add config-set

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package kiicli
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/tmtk75/cli"
 )
@@ -14,12 +15,31 @@ func PrintAppInfo() {
 	fmt.Printf("%s\n", string(body))
 }
 
+func SetValueInAppConfig(prop, val string) {
+	p := Profile()
+	path := fmt.Sprintf("/apps/%s/configuration/parameters/%s", p.AppId, prop)
+	headers := p.HttpHeadersWithAuthorization("text/plain")
+	body := HttpPut(path, headers, strings.NewReader(val)).Bytes()
+	fmt.Printf("%s\n", string(body))
+}
+
 var AppCommands = []cli.Command{
 	{
 		Name:  "config",
 		Usage: "Print config of app",
 		Action: func(c *cli.Context) {
 			PrintAppInfo()
+		},
+	},
+	{
+		Name:        "config-set",
+		Usage:       "Set a value in a property of config",
+		Args:        "<property> <value>",
+		Description: `   e.g) phoneNumberVerificationRequired true`,
+		Action: func(c *cli.Context) {
+			prop, _ := c.ArgFor("property")
+			val, _ := c.ArgFor("value")
+			SetValueInAppConfig(prop, val)
 		},
 	},
 }
