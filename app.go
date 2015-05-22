be21585f6b0403a5493e1101a0ae1c3dd4abd6e5
jolivares <juan.o.lazcano@gmail.com>
2015-05-22 22:36:07 +0200

    allow to set app params

--- app.go
+++ app.go
@@ -2,8 +2,8 @@ package kiicli
 
 import (
 	"fmt"
-
 	"github.com/tmtk75/cli"
+	"strings"
 )
 
 func PrintAppInfo() {
@@ -14,12 +14,30 @@ func PrintAppInfo() {
 	fmt.Printf("%s\n", string(body))
 }
 
+func SetAppParam(name string, value string) {
+	p := Profile()
+	path := fmt.Sprintf("/apps/%s/configuration/parameters/%s", p.AppId, name)
+	headers := p.HttpHeadersWithAuthorization("text/plain")
+	body := HttpPut(path, headers, strings.NewReader(value)).Bytes()
+	fmt.Printf("%s\n", string(body))
+}
+
 var AppCommands = []cli.Command{
 	{
 		Name:  "config",
 		Usage: "Print config of app",
 		Action: func(c *cli.Context) {
 			PrintAppInfo()
+		},
+	},
+	{
+		Name:  "set-param",
+		Usage: "set an app param",
+		Args:  "<paramname> <paramvalue>",
+		Action: func(c *cli.Context) {
+			name, _ := c.ArgFor("paramname")
+			value, _ := c.ArgFor("paramvalue")
+			SetAppParam(name, value)
 		},
 	},
 }
